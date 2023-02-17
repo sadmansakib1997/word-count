f1be3aa6dc2f9ea852d202201002a4340f86e70d
sadmansakib1997 <sadmansakib670@gmail.com>
2023-02-17 16:30:06 +0600

    sakib

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 import From from "./Components/From";
+import { useState } from "react";
 
 function App() {
+  const [istoggle, setToggled] = useState(false);
   return (
     <div className="App">
+      <button onClick={() => setToggled(!istoggle)}>SAKib</button>
+      {istoggle ? <h1>sakib</h1> : <h1>Synthia</h1>}
+
       <From></From>
     </div>
   );
